8ef0fe18176eb019f3da7f0345cda67f3d15bd33
Purpose-Dev <riyane.dev@gmail.com>
2024-11-23 23:02:20 +0100

    feat(linting): add ESLint configuration for TypeScript
    ---
    Added a new ESLint configuration file `eslint.config.mjs` with TypeScript support using `@typescript-eslint/parser` and `@typescript-eslint/eslint-plugin`. Custom rules and ignored directories are specified.

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import * as tsParser from '@typescript-eslint/parser';
+import * as tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+    {
+        files: ['src/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+        },
+        rules: {
+            'no-async-promise-executor': 'off',
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-var-requires': 'error',
+        },
+        ignores: [
+            '.github',
+            'node_modules',
+            'dist'
+        ],
+    },
+];
